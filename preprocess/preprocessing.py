bb37faaa36a3d0846454524b7f1a1ee28845ca77
Dipesh Silwal <75604769+dipesg@users.noreply.github.com>
2022-04-20 07:35:52 +0545

    Update preprocessing.py

--- preprocess/preprocessing.py
+++ preprocess/preprocessing.py
@@ -11,8 +11,6 @@ class Process:
         self.helper = Helper()
         self.movie = pd.read_csv('data/movies.csv')
         self.credits = pd.read_csv('data/credits.csv')
-        
-        # Initializing the logger object
         self.file_object = open("Logs/preprocessing_log.txt", 'a+')
         self.log_writer = logger.App_Logger()
         
@@ -29,8 +27,6 @@ class Process:
     def select_columns(self):
         try:
             self.merged_dataframe = self.merge_dataframe()
-            
-            # Keeping important columns for recommendation
             self.merged_dataframe = self.merged_dataframe[['movie_id','title','overview','genres','keywords','cast','crew']]
             return self.merged_dataframe
             
@@ -55,15 +51,8 @@ class Process:
     def convert_to_list(self):
         try:
             self.cleaned_dataframe = self.drop_outliers()
-            
-            # Converting genres column to list of words
             self.cleaned_dataframe['genres'] = self.cleaned_dataframe['genres'].apply(self.helper.convert)
-            #return self.movies
-            
-            # Converting keywords column to list of words
             self.cleaned_dataframe['keywords'] = self.cleaned_dataframe['keywords'].apply(self.helper.convert)
-            
-            # Converting cast column to list of words
             self.cleaned_dataframe['cast'] = self.cleaned_dataframe['cast'].apply(self.helper.convert_cast)
             
             # Fetching director name from crew column.
@@ -81,7 +70,6 @@ class Process:
         try:   
             self.converted_to_list_dataframe = self.convert_to_list()
         
-            # Removing space from cast, crew, genres and keywords column.
             self.converted_to_list_dataframe['cast'] = self.converted_to_list_dataframe['cast'].apply(self.helper.remove_space)
             self.converted_to_list_dataframe['crew'] = self.converted_to_list_dataframe['crew'].apply(self.helper.remove_space)
             self.converted_to_list_dataframe['genres'] = self.converted_to_list_dataframe['genres'].apply(self.helper.remove_space)
@@ -98,7 +86,6 @@ class Process:
         try:
             self.space_removed_dataframe = self.remove_space()
             
-            # Concatinate all
             self.space_removed_dataframe['tags'] = str(self.space_removed_dataframe['overview']) + str(self.space_removed_dataframe['genres']) + str(self.space_removed_dataframe['keywords']) + str(self.space_removed_dataframe['cast']) + str(self.space_removed_dataframe['crew'])
             return self.space_removed_dataframe
         
@@ -110,7 +97,6 @@ class Process:
         try:
             self.new_dataframe = self.concat_all()
             
-            # Selecting only required columns and creating a new dataframe.
             self.new_dataframe = self.new_dataframe[['movie_id','title','tags']]
             return self.new_dataframe
             
